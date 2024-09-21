53f5cb15ad5a7335b5c8a60dafb344f4ea38986f
CristhianStorm <139923558+CristhianStorm@users.noreply.github.com>
2024-09-21 13:58:17 -0400

    Add files via upload

---
+++ Python/clase2/condicionales.py
@@ -0,0 +1,14 @@
+'''
+Condicionales if
+'''
+dato= int(input("Ingrese un numero: "))
+
+if (dato > 0):
+    print("Numero positivo")
+elif dato ==0:
+    print("Resultado igual a cero")
+else:
+    print("Numero negativo")
+
+#Indentacion
+    
